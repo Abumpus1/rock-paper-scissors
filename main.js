3e5043892c95886b3f2c219bbae1e331e79f9b5e
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-03-04 16:58:09 -0700

    Set up initial query selectors

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
+// variables
+var humanWins = document.querySelector("#humanWins");
+var computerWins = document.querySelector("#computerWins");
+var optionContainer = document.querySelector(".rock-paper-scissors-container");
+var pickOptions = document.querySelectorAll(".pick-option");
+
 var game = new Game;
 console.log(game);
+
+// event listeners
+
+
+// functions
