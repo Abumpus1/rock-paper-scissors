1eab2fdda027101276e6be245eea073722fe16dc
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-03-04 20:19:42 -0700

    Allow user to select classic game

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
 // variables
 var humanWins = document.querySelector("#humanWins");
 var computerWins = document.querySelector("#computerWins");
-var optionsContainer = document.querySelector(".rock-paper-scissors-container");
 var pickOptions = document.querySelectorAll(".pick-option");
+
+var gameSelectContainer = document.querySelector(".game-select-container");
+var optionsContainer = document.querySelector(".rock-paper-scissors-container");
 
 var game = new Game;
 console.log(game);
 
 // event listeners
-optionsContainer.addEventListener('click', function(event) {
-  clickWeapon(event);
+gameSelectContainer.addEventListener("click", function(event) {
+  if (event.target.className === "game-option") {
+    chooseGame(event.target.id)
+  }
+});
+optionsContainer.addEventListener("click", function(event) {
+  if (event.target.className === "pick-option") {
+    chooseWeapons(event.target.id);
+  }
 });
 
 // functions
-function clickWeapon(event) {
-  if (event.target.className === "pick-option") {
-    chooseWeapons(event.target)
-  }
+function hide(element) {
+  element.classList.add("hidden");
 }
 
-function chooseWeapons(clickedImage) {
-  game.takeTurns(clickedImage.id);
+function show(element) {
+  element.classList.remove("hidden");
+}
+
+function chooseGame(gameSelected) {
+  game.gameType = gameSelected;
+  hide(gameSelectContainer);
+  show(optionsContainer);
+}
+
+function chooseWeapons(weapon) {
+  game.takeTurns(weapon);
   humanWins.innerText = `Wins: ${game.players.human.wins}`;
   computerWins.innerText = `Wins: ${game.players.computer.wins}`;
 }
