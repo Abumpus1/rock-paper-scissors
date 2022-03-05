cc4edcb26998f5c3df195791e97ad75c711b08c5
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-03-04 17:19:02 -0700

    Allow player to click option and auto update score

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 // variables
 var humanWins = document.querySelector("#humanWins");
 var computerWins = document.querySelector("#computerWins");
-var optionContainer = document.querySelector(".rock-paper-scissors-container");
+var optionsContainer = document.querySelector(".rock-paper-scissors-container");
 var pickOptions = document.querySelectorAll(".pick-option");
 
 var game = new Game;
 console.log(game);
 
 // event listeners
-
+optionsContainer.addEventListener('click', function(event) {
+  clickWeapon(event);
+});
 
 // functions
+function clickWeapon(event) {
+  if (event.target.className === "pick-option") {
+    chooseWeapon(event.target)
+  }
+}
+
+function chooseWeapon(clickedImage) {
+  game.takeTurns(clickedImage.id);
+  humanWins.innerText = `Wins: ${game.players.human.wins}`;
+  computerWins.innerText = `Wins: ${game.players.computer.wins}`;
+  // for (var i = 0; i < pickOptions.length; i++) {
+  //   if (clickedImage.id === ) {
+  //
+  //   }
+  // }
+}
